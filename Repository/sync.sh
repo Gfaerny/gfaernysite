6ecc9c06fd7e4b40cafa61fb118785b8c04a3ee5
gfaerny <gfaerny@gmail.com>
2025-09-22 22:20:40 +0330

    add repository to host and init css file manully
    I css file to to audio or programing  for projects index's file
    and i try to add my fav repository to this site source for future i want to make them in my site visible

---
+++ Repository/sync.sh
@@ -0,0 +1,23 @@
+cd clc.
+gh repo sync
+cd ..
+
+cd sennlib
+gh repo sync
+cd ..
+
+cd gfaernysite
+gh repo sync
+cd ..
+
+cd TASK
+gh repo sync
+cd ..
+
+cd tng
+gh repo sync
+cd ..
+
+cd focus-editor-scooplink
+gh repo sync
+cd ..
